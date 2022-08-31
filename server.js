c0b0990bd6d7a38777b7a8469ab35d392b6d2b10
Krystian Florkowski <86536950+Krytianf06@users.noreply.github.com>
2022-08-31 19:40:17 +0200

    Koniec hw03
    Do sprawdzenia

--- server.js
+++ server.js
@@ -1,5 +1,25 @@
 const app = require('./app')
+const mongoose = require('mongoose')
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3001;
+const uriDb = process.env.DB_SRV;
+
+
+const connection = mongoose.connect(uriDb, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+connection
+  .then(() => {
+    console.log("Database connection successful");
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Server not running", err.toString());
+    process.exit(1);
+  });
